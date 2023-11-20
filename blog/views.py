41a173635a9706db3b1ec0f86e9858ba2f7efbae
Heqing Huang <kirsch0421@hotmail.com>
2023-11-19 22:16:57 -0800

    add comments and display comments

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+
+from .forms import CommentForm 
 
 # Create your views here.
+
+def detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+
+    if request.method == 'Post':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', slug=slug)
+    else: 
+        form = CommentForm()
+
+    return render(request, 'blog/detail.html', {'post': post, 'form' : form})
